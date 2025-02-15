9ee53affc233217bc0a1abbdc39d3c99b229fcba
vdumitrache <valentin99dumitrache@gmail.com>
2025-02-15 18:22:01 +0200

    fix lint error

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,6 +2,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (req.method === 'GET') {
         const { param1, param2 } = req.query;
         res.status(200).json({ message: 'Test GET request successful', param1, param2 });
