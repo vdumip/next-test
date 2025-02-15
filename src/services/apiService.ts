3b520f31763bce4b1dfe3dc87624f9c4b985bbd1
vdumitrache <valentin99dumitrache@gmail.com>
2025-02-15 18:19:19 +0200

    fix lint error

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,7 @@
 // src/services/apiService.ts
 import { Post } from 'types/Post';
 
-export async function fetchTestData(): Promise<Post> {
+export async function fetchTestData(): Promise<Post[]> {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const testResponse = await fetch(`http://localhost:3000/api/test?param1=test&param2=anotherTest`);
     if(testResponse.ok) {
